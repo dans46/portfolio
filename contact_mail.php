25169e97f3df5b5941c247ab9b3b4bda99a636ac
dans46 <62874416+dans46@users.noreply.github.com>
2020-05-10 10:45:18 +0200

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -2,8 +2,8 @@
 $toEmail = "hello@danielsabol.com";
 $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
 if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
-print "<p class='success'>Contact Mail Sent.</p>";
+print "<p class='success'>Ďakujeme za Vašu správu</p>";
 } else {
-print "<p class='Error'>Problem in Sending Mail.</p>";
+print "<p class='Error'>Problém s odoslaním, skúste prosím neskôr.</p>";
 }
 ?>
